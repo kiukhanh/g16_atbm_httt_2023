1a15b579c2f44cc8f9de54356086e420503d9dfa
baoBao021212 <115094808+baoBao021212@users.noreply.github.com>
2023-06-27 21:45:22 +0700

    update câu lệnh xem lịch sử audit

--- ATBM/Phanhe1/Trưởng phòng/Form_audit.cs
+++ ATBM/Phanhe1/Trưởng phòng/Form_audit.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,6 +14,7 @@ namespace Phanhe1
     public partial class Form_audit : Form
     {
         DataTable dtTableName = new DataTable();
+        DataTable dtTableName2 = new DataTable();
         public Form_audit()
         {
             InitializeComponent();
@@ -20,9 +22,13 @@ namespace Phanhe1
 
         private void Load_data()
         {
-            string sql = "SELECT * FROM DBA_COMMON_AUDIT_TRAIL";
+            string sql = "SELECT * FROM UNIFIED_AUDIT_TRAIL WHERE AUDIT_TYPE = 'FineGrainedAudit'";
             dtTableName = Connectionfunction.GetDataToTable(sql);
-            dataGridView1.DataSource = dtTableName;
+            dataGridView2.DataSource = dtTableName;
+
+            string sql2 = "SELECT * FROM UNIFIED_AUDIT_TRAIL WHERE AUDIT_TYPE = 'Standard'";
+            dtTableName2 = Connectionfunction.GetDataToTable(sql2);
+            dataGridView1.DataSource = dtTableName2;
         }
         private void Form_View_User_Load(object sender, EventArgs e)
         {
--- ATBM/Phanhe1/Trưởng phòng/Form_audit.designer.cs
+++ ATBM/Phanhe1/Trưởng phòng/Form_audit.designer.cs
@@ -30,7 +30,11 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -49,13 +53,48 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(33, 92);
+            this.dataGridView1.Location = new System.Drawing.Point(33, 122);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1111, 484);
+            this.dataGridView1.Size = new System.Drawing.Size(1136, 214);
             this.dataGridView1.TabIndex = 2;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AllowUserToAddRows = false;
+            this.dataGridView2.AllowUserToDeleteRows = false;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(33, 397);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.ReadOnly = true;
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(1136, 194);
+            this.dataGridView2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(177)))), ((int)(((byte)(10)))));
+            this.label2.Location = new System.Drawing.Point(28, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 30);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "STANDARD";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(177)))), ((int)(((byte)(10)))));
+            this.label3.Location = new System.Drawing.Point(28, 364);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 30);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "FGA";
             // 
             // Form_audit
             // 
@@ -63,12 +102,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(95)))), ((int)(((byte)(87)))));
             this.ClientSize = new System.Drawing.Size(1181, 603);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label1);
             this.Name = "Form_audit";
             this.Text = "Form_audit";
             this.Load += new System.EventHandler(this.Form_View_User_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,5 +120,8 @@
         #endregion
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
